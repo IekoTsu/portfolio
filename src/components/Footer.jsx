0d6a351f5079738cd4b2518f021af893d91a0ab7
IekoTsu <bahamilouah@gmail.com>
2024-05-04 18:50:43 +0200

    Page spacing right fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,49 +43,59 @@ const Footer = () => {
     return(
         <footer className="footer">
             <UpBtn></UpBtn>
-            <div className="footer__content row gap-sm-2 gap-md-0 gap-lg-0 p-5">
-                <div className="footer__section col-lg-3 col-md-6">
-                    <h5 className="footer__title">John Doe</h5>
-                    <ul className="footer__list list-unstyled">
-                        <li className="footer__item d-flex align-items-center">
-                            <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.7786657,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?authuser=0&entry=ttu" target="_blank">
-                                40 Rue Laure Diebold <br/> 69009 Lyon, France 
-                             </a> 
-                        </li>
-                        <li className="footer__item d-flex align-items-center"> <a className="footer__link" href="tel:+33620304050" target="_blank">Téléphone: 06 20 30 40 50 </a> </li>
-                        <li className="footer__social-icons my-3">
-                            <Link to='/github-profile'><i className="footer__icon fa-brands fa-github fa-2xl m-3"></i></Link>
-                            <a href="https://fr.wikipedia.org/wiki/John_Doe" target="_blank"><i className="footer__icon fa-brands fa-square-twitter fa-2xl m-3"></i></a>
-                            <a href="https://fr.wikipedia.org/wiki/John_Doe" target="_blank"><i className="footer__icon fa-brands fa-linkedin fa-2xl m-3"></i></a>
-                        </li>
-                    </ul>
+            <div className="footer__content p-5">
+            <div className="row">
+                <div className="col-lg-3 col-md-6">
+                    <div className="footer__section ">
+                        <h5 className="footer__title">John Doe</h5>
+                        <ul className="footer__list list-unstyled">
+                            <li className="footer__item d-flex align-items-center">
+                                <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.7786657,4.796404,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?authuser=0&entry=ttu" target="_blank">
+                                    40 Rue Laure Diebold <br/> 69009 Lyon, France 
+                                 </a> 
+                            </li>
+                            <li className="footer__item d-flex align-items-center"> <a className="footer__link" href="tel:+33620304050" target="_blank">Téléphone: 06 20 30 40 50 </a> </li>
+                            <li className="footer__social-icons my-3">
+                                <Link to='/github-profile'><i className="footer__icon fa-brands fa-github fa-2xl m-3"></i></Link>
+                                <a href="https://fr.wikipedia.org/wiki/John_Doe" target="_blank"><i className="footer__icon fa-brands fa-square-twitter fa-2xl m-3"></i></a>
+                                <a href="https://fr.wikipedia.org/wiki/John_Doe" target="_blank"><i className="footer__icon fa-brands fa-linkedin fa-2xl m-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer__section col-lg-3  col-md-6">
-                    <h5 className="footer__title">Liens utiles</h5>
-                    <ul className="footer__list list-unstyled">
-                        <li className="footer__item d-flex align-items-top"><Link to='/'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Accueil </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/' href='#scrollspyAbout'><i className="fa-solid fa-angle-right me-1 pt-1"></i> À propos </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/services'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Services </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/contact'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Me contacter </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to="/legal-notices"><i className="fa-solid fa-angle-right me-1 pt-1"></i> Mentions légales</Link> </li>
-                    </ul>
+                <div className="col-lg-3  col-md-6">
+                    <div className="footer__section">
+                        <h5 className="footer__title">Liens utiles</h5>
+                        <ul className="footer__list list-unstyled">
+                            <li className="footer__item d-flex align-items-top"><Link to='/'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Accueil </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/' href='#scrollspyAbout'><i className="fa-solid fa-angle-right me-1 pt-1"></i> À propos </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/services'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Services </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/contact'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Me contacter </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to="/legal-notices"><i className="fa-solid fa-angle-right me-1 pt-1"></i> Mentions légales</Link> </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer__section col-lg-3  col-md-6">
-                    <h5 className="footer__title" >Mes dernières réalisations</h5>
-                    <ul className="footer__list list-unstyled">
-                        <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Fresh food </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Restaurant Akira </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Espace bien-être </Link></li>
-                    </ul>
+                <div className="col-lg-3  col-md-6">
+                    <div className="footer__section">
+                        <h5 className="footer__title" >Mes dernières réalisations</h5>
+                        <ul className="footer__list list-unstyled">
+                            <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Fresh food </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Restaurant Akira </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/achievements'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Espace bien-être </Link></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="footer__section col-lg-3  col-md-6">
-                    <h5 className="footer__title" >Mes dernires artiles</h5>
-                    <ul className="footer__list list-unstyled">
-                        <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Coder son site en HTML/CSS </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Vendre ses produits sur le web </Link></li>
-                        <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Se positionner sur Google </Link></li>
-                    </ul>
+                <div className="col-lg-3  col-md-6">
+                    <div className="footer__section">
+                        <h5 className="footer__title" >Mes dernires artiles</h5>
+                        <ul className="footer__list list-unstyled">
+                            <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Coder son site en HTML/CSS </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Vendre ses produits sur le web </Link></li>
+                            <li className="footer__item d-flex align-items-top"><Link to='/blog'><i className="fa-solid fa-angle-right me-1 pt-1"></i> Se positionner sur Google </Link></li>
+                        </ul>
+                    </div>
                 </div>
+            </div>
             </div>
             <div className="footer__copyright">
                 <p className="footer__text text-light text-center p-3"> <i className="fa-regular fa-copyright"></i> Designed by John Doe</p>
